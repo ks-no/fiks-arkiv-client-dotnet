8c10f0adc2da19c8e264ddcb272b82f04d90cbba
Jarle Børsheim <jarle.borsheim@gmail.com>
2021-12-01 15:34:17 +0100

    Hent er .innsyn og arkivmelding er .arkivering

--- KS.Fiks.IO.Arkiv.Client/Models/ArkivintegrasjonMeldingTypeV1.cs
+++ KS.Fiks.IO.Arkiv.Client/Models/ArkivintegrasjonMeldingTypeV1.cs
@@ -8,18 +8,18 @@ namespace KS.Fiks.IO.Arkiv.Client.Models
         public const string Mottatt = "no.ks.fiks.arkiv.v1.mottatt";
         public const string Kvittering = "no.ks.fiks.arkiv.v1.kvittering";
         
-        // Arkivmelding
-        public const string Arkivmelding = "no.ks.fiks.arkiv.v1.arkivmelding";
+        // Arkivering
+        public const string Arkivmelding = "no.ks.fiks.arkiv.v1.arkivering.arkivmelding";
         
-        //Hent
-        public const string MappeHent = "no.ks.fiks.arkiv.v1.mappe.hent";
-        public const string MappeHentResultat = "no.ks.fiks.arkiv.v1.mappe.hent.resultat";
-        public const string JournalpostHent = "no.ks.fiks.arkiv.v1.journalpost.hent";
-        public const string JournalpostHentResultat = "no.ks.fiks.arkiv.v1.journalpost.hent.resultat";
-        public const string DokumentfilHent = "no.ks.fiks.arkiv.v1.dokumentfil.hent";
-        public const string DokumentfilHentResultat = "no.ks.fiks.arkiv.v1.dokumentfil.hent.resultat";
+        // Innsyn Hent
+        public const string MappeHent = "no.ks.fiks.arkiv.v1.innsyn.mappe.hent";
+        public const string MappeHentResultat = "no.ks.fiks.arkiv.v1.innsyn.mappe.hent.resultat";
+        public const string JournalpostHent = "no.ks.fiks.arkiv.v1.innsyn.journalpost.hent";
+        public const string JournalpostHentResultat = "no.ks.fiks.arkiv.v1.innsyn.journalpost.hent.resultat";
+        public const string DokumentfilHent = "no.ks.fiks.arkiv.v1.innsyn.dokumentfil.hent";
+        public const string DokumentfilHentResultat = "no.ks.fiks.arkiv.v1.innsyn.dokumentfil.hent.resultat";
         
-        // Sok
+        // Innsyn Søk
         public const string Sok = "no.ks.fiks.arkiv.v1.innsyn.sok";
         public const string SokResultatUtvidet = "no.ks.fiks.arkiv.v1.innsyn.sok.resultat.utvidet";
         public const string SokResultatMinimum = "no.ks.fiks.arkiv.v1.innsyn.sok.resultat.minimum";
