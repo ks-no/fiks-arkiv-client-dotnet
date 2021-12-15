00e5b0d783d40c8141bb4be7e543507d83861872
Jarle Børsheim <jarle.borsheim@gmail.com>
2021-12-15 15:25:35 +0100

    Nådde threshold for antall ignore på tester. Doh...

--- KS.Fiks.IO.Arkiv.Client.Integration.Tests/KS.Fiks.IO.Arkiv.Client.Integration.Tests/SchemaTester/GenererKodeFraXsdTester.cs
+++ KS.Fiks.IO.Arkiv.Client.Integration.Tests/KS.Fiks.IO.Arkiv.Client.Integration.Tests/SchemaTester/GenererKodeFraXsdTester.cs
@@ -8,16 +8,20 @@ namespace KS.Fiks.IO.Arkiv.Client.Integration.Tests.SchemaTester
     [TestFixture]
     public class GenererKodeFraXsdTester
     {
-        [Test] 
+        [Test]
+        [Ignore("Kjøres manuelt ved behov da den ikke kan kjøres på Jenkins")]
         [TestCase("arkivmelding.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding")]
         [TestCase("arkivmeldingKvittering.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.kvittering")]
         [TestCase("sok.xsd /c /n:no.ks.fiks.io.arkivmelding.sok")]
-        [TestCase("sokeresultatMinimum.xsd arkivstrukturMinimum.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.sok.resultat")]
-        [TestCase("sokeresultatNoekler.xsd arkivstrukturNoekler.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.sok.resultat")]
-        [TestCase("sokeresultatUtvidet.xsd arkivstruktur.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.sok.resultat")]
-        [TestCase("dokumentfilHent.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.dokument.hent")]
-        [TestCase("journalpostHent.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.journalpost.hent")]
-        [TestCase("mappeHent.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.mappe.hent")]
+        [TestCase("sokeresultatMinimum.xsd arkivstrukturMinimum.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.innsyn.sok.resultat.minimum")]
+        [TestCase("sokeresultatNoekler.xsd arkivstrukturNoekler.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.innsyn.sok.resultat.noekler")]
+        [TestCase("sokeresultatUtvidet.xsd arkivstruktur.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.innsyn.sok.resultat.utvidet")]
+        [TestCase("dokumentfilHent.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.innsyn.dokumentfil.hent")]
+        [TestCase("journalpostHent.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.innsyn.journalpost.hent")]
+        [TestCase("journalpostHentResultat.xsd metadatakatalog.xsd arkivmelding.xsd /c /n:no.ks.fiks.io.arkivmelding.innsyn.journalpost.hent.resultat")]
+        [TestCase("mappeHent.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.innsyn.mappe.hent")]
+        [TestCase("mappeHentResultat.xsd metadatakatalog.xsd arkivmelding.xsd /c /n:no.ks.fiks.io.arkivmelding.innsyn.mappe.hent.resultat")]
+        [TestCase("arkivmelding.xsd arkivmeldingKvittering.xsd dokumentfilHent.xsd journalpostHent.xsd journalpostHentResultat.xsd mappeHent.xsd mappeHentResultat.xsd metadatakatalog.xsd sokeresultatMinimum.xsd arkivstrukturMinimum.xsd sokeresultatNoekler.xsd arkivstrukturNoekler.xsd sokeresultatUtvidet.xsd /c /n:no.ks.fiks.io.arkiv")]
         public void Generer_fra_xsd(string arguments)
         {
             //TODO Denne testen klarer ikke å bli kjørt på Jenkins. Mest sannsynlig fordi xsd.exe ikke er tilgjengelig som kommando. 
--- KS.Fiks.IO.Arkiv.Client.Tests/GenererKodeFraXsdTester.cs
+++ KS.Fiks.IO.Arkiv.Client.Tests/GenererKodeFraXsdTester.cs
@@ -8,19 +8,10 @@ namespace KS.Fiks.IO.Arkiv.Client.Integration.Tests.SchemaTester
     [TestFixture]
     public class GenererKodeFraXsdTester
     {
+        
         [Test]
         [Ignore("Kjøres manuelt ved behov da den ikke kan kjøres på Jenkins")]
-        [TestCase("arkivmelding.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding")]
-        [TestCase("arkivmeldingKvittering.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.kvittering")]
         [TestCase("sok.xsd /c /n:no.ks.fiks.io.arkivmelding.sok")]
-        [TestCase("sokeresultatMinimum.xsd arkivstrukturMinimum.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.innsyn.sok.resultat.minimum")]
-        [TestCase("sokeresultatNoekler.xsd arkivstrukturNoekler.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.innsyn.sok.resultat.noekler")]
-        [TestCase("sokeresultatUtvidet.xsd arkivstruktur.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.innsyn.sok.resultat.utvidet")]
-        [TestCase("dokumentfilHent.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.innsyn.dokumentfil.hent")]
-        [TestCase("journalpostHent.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.innsyn.journalpost.hent")]
-        [TestCase("journalpostHentResultat.xsd metadatakatalog.xsd arkivmelding.xsd /c /n:no.ks.fiks.io.arkivmelding.innsyn.journalpost.hent.resultat")]
-        [TestCase("mappeHent.xsd metadatakatalog.xsd /c /n:no.ks.fiks.io.arkivmelding.innsyn.mappe.hent")]
-        [TestCase("mappeHentResultat.xsd metadatakatalog.xsd arkivmelding.xsd /c /n:no.ks.fiks.io.arkivmelding.innsyn.mappe.hent.resultat")]
         [TestCase("arkivmelding.xsd arkivmeldingKvittering.xsd dokumentfilHent.xsd journalpostHent.xsd journalpostHentResultat.xsd mappeHent.xsd mappeHentResultat.xsd metadatakatalog.xsd sokeresultatMinimum.xsd arkivstrukturMinimum.xsd sokeresultatNoekler.xsd arkivstrukturNoekler.xsd sokeresultatUtvidet.xsd /c /n:no.ks.fiks.io.arkiv")]
         public void Generer_fra_xsd(string arguments)
         {
