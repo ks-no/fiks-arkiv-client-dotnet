3069842a223215f528024476ce67ec03fa3a3e66
Jarle Børsheim <jarle.borsheim@gmail.com>
2021-12-15 10:34:31 +0100

    Tatt bort xsd.exe testen

--- KS.Fiks.IO.Arkiv.Client.Tests/GenererKodeFraXsdTester.cs
+++ KS.Fiks.IO.Arkiv.Client.Tests/GenererKodeFraXsdTester.cs
@@ -11,56 +11,6 @@ namespace KS.Fiks.IO.Arkiv.Client.Integration.Tests.SchemaTester
     public class GenerateWithXsdExe
     {
         [Test]
-        [Ignore("Kjøres manuelt ved behov da den ikke kan kjøres på Jenkins")]
-        [TestCase("arkivmelding.xsd metadatakatalog.xsd /c /n:no.ks.fiks.arkiv.v1.arkivering.arkivmelding")]
-        [TestCase("arkivmeldingKvittering.xsd metadatakatalog.xsd /c /n:no.ks.fiks.arkiv.v1.arkivering.arkivmelding.kvittering")]
-        [TestCase("sok.xsd /c /n:no.ks.fiks.arkiv.v1.innsyn.sok")]
-        [TestCase("sokeresultatMinimum.xsd arkivstrukturMinimum.xsd metadatakatalog.xsd /c /n:no.ks.fiks.arkiv.v1.innsyn.sok.resultat")]
-        [TestCase("sokeresultatNoekler.xsd arkivstrukturNoekler.xsd metadatakatalog.xsd /c /n:no.ks.fiks.arkiv.v1.innsyn.sok.resultat")]
-        [TestCase("sokeresultatUtvidet.xsd arkivstruktur.xsd metadatakatalog.xsd /c /n:no.ks.fiks.arkiv.v1.innsyn.sok.resultat")]
-        [TestCase("dokumentfilHent.xsd metadatakatalog.xsd /c /n:no.ks.fiks.arkiv.v1.innsyn.dokumentfil.hent")]
-        [TestCase("journalpostHent.xsd metadatakatalog.xsd /c /n:no.ks.fiks.arkiv.v1.innsyn.journalpost.hent")]
-        [TestCase("journalpostHentResultat.xsd arkivmelding.xsd metadatakatalog.xsd /c /n:no.ks.fiks.arkiv.v1.innsyn.journalpost.hent.resultat")]
-        [TestCase("mappeHent.xsd metadatakatalog.xsd /c /n:no.ks.fiks.arkiv.v1.innsyn.mappe.hent")]
-        [TestCase("mappeHentResultat.xsd arkivmelding.xsd metadatakatalog.xsd /c /n:no.ks.fiks.arkiv.v1.innsyn.mappe.hent.resultat")]
-        public void Generer_fra_xsd(string arguments)
-        {
-            //TODO Denne testen klarer ikke å bli kjørt på Jenkins. Mest sannsynlig fordi xsd.exe ikke er tilgjengelig som kommando. 
-            var workingDir = Directory.GetCurrentDirectory() + "/Schema";
-            var process = new Process
-            {
-                StartInfo =
-                {
-                    FileName = "xsd",
-                    Arguments = arguments,
-                    RedirectStandardInput = true,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true,
-                    UseShellExecute = false,
-                    ErrorDialog = false,
-                    WorkingDirectory = workingDir
-                },
-                EnableRaisingEvents = true,
-            };
-            process.Start();
-            
-            var stdout = process.StandardOutput.ReadToEnd();
-            
-            process.StandardInput.Flush();
-            process.StandardInput.Close();
-            process.WaitForExit(5000);
-
-            var exitCode = process.ExitCode;
-            
-            Console.Out.WriteLine(stdout);
-           
-            Assert.True(exitCode == 0, $"Feilet med exitCode {exitCode}. Se output for detaljer.");
-            Assert.False(stdout.ToLower().Contains("exception"), $"Exception melding i output. Sjekk output for mer detaljer."); 
-            Assert.False(stdout.Contains("Error"), $"Error melding i output. Sjekk output for mer detaljer."); 
-            Assert.False(stdout.Contains("Warning"),$"Warning melding i output. Sjekk output for mer detaljer."); 
-        }
-
-        [Test]
         public void GenerateWithXmlSchemaClassGenerator()
         {
             var fileList = new List<string>
