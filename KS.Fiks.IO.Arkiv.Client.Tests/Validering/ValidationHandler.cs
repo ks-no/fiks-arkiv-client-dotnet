da9a175e61152653d1dfc4a2da29c84490c2cc38
Jarle Børsheim <jarle.borsheim@gmail.com>
2022-02-23 11:49:53 +0100

    Validering helpers

---
+++ KS.Fiks.IO.Arkiv.Client.Tests/Validering/ValidationHandler.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Xml.Schema;
+
+namespace KS.Fiks.IO.Arkiv.Client.Tests.Validering
+{
+    public class ValidationHandler
+    {
+        public int numberOfErrors = 0;
+
+        public void HandleValidationError(object sender, ValidationEventArgs e)
+        {
+            if (e.Severity == XmlSeverityType.Warning)
+            {
+                Console.Write("WARNING: ");
+                Console.WriteLine(e.Message);
+            }
+            else if (e.Severity == XmlSeverityType.Error)
+            {
+                numberOfErrors++;
+                Console.Write("ERROR: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public bool HasErrors()
+        {
+            return numberOfErrors > 0;
+        } 
+    }
+}
