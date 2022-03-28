223043d06b37b815868942899fc95bfac27cd799
Jarle Børsheim <jarle.borsheim@gmail.com>
2022-03-28 10:28:32 +0200

    Fikset DokumenttypeKoder etter møte og lagt på sjekk i constructor for Kode

--- KS.Fiks.IO.Arkiv.Client/Models/Kodelister/Kode.cs
+++ KS.Fiks.IO.Arkiv.Client/Models/Kodelister/Kode.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace KS.Fiks.IO.Arkiv.Client.Models.Kodelister
 {
     public class Kode
@@ -7,6 +9,10 @@ namespace KS.Fiks.IO.Arkiv.Client.Models.Kodelister
 
         public Kode(string Verdi, string Beskrivelse)
         {
+            if (string.IsNullOrWhiteSpace(Verdi))
+            {
+                throw new ArgumentException("Kode må ha en verdi");
+            }
             this.Verdi = Verdi;
             this.Beskrivelse = Beskrivelse;
         }
